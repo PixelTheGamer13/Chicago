be5f92153966bf40cd91c22929ed684d89a19d93
Viktor Buska <viktor.b-elev1@utb.hudiksvall.se>
2024-01-19 15:58:37 +0100

    Add project files.

---
+++ Chicago/Kortlek.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chicago
+{
+    class Kortlek
+    {
+        public List<string> skapaKortlek()
+        {
+            List<string> skapakortlek = new List<string>();
+            string[] färger = new string[4] { "H", "R", "K", "S" };
+            string kort;
+
+            for (int i = 1; i < 14; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    switch (i)
+                    {
+                        case 1:
+                            kort = färger[j] + "A";
+                            skapakortlek.Add(kort);
+                            break;
+
+                        case 10:
+                            kort = färger[j] + "T";
+                            skapakortlek.Add(kort);
+                            break;
+
+                        case 11:
+                            kort = färger[j] + "J";
+                            skapakortlek.Add(kort);
+                            break;
+
+                        case 12:
+                            kort = färger[j] + "D";
+                            skapakortlek.Add(kort);
+                            break;
+
+                        case 13:
+                            kort = färger[j] + "K";
+                            skapakortlek.Add(kort);
+                            break;
+
+                        default:
+                            kort = färger[j] + i.ToString();
+                            skapakortlek.Add(kort);
+                            break;
+                    }
+                }
+            }
+            return skapaKortlek();
+        }
+    }
+}
---
+++ Chicago/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chicago
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Kortlek kortlek = new Kortlek();
+        List<string> kortlista = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_DelaUt_Click(object sender, RoutedEventArgs e)
+        {
+            kortlista = kortlek.skapaKortlek();
+            int y = 1;
+            for (int i = 0; i < 20; i++)
+            {
+                Random rnd = new Random();
+                int x = rnd.Next(kortlista.Count);
+                if (i < 5)
+                {
+                    string controlName = $"a{y}b";
+                    Image imgControl = FindName(controlName) as Image;
+                    if (imgControl != null)
+                    {
+                        string imageName = @"C:\Users\viktor.b - elev1\Pictures\Kort\" + kortlista[x] + ".png";
+                        Uri imageUri = new Uri(imageName);
+
+                        imgControl.Source = new BitmapImage(imageUri);
+
+                    }
+                }
+                else if (i < 10)
+                {
+
+                }
+                else if (i < 15)
+                {
+
+                }
+                else
+                {
+
+                }
+                if (y < 5) y++;
+                else y = 1;
+                kortlista.RemoveAt(x);
+            }
+        }
+    }
+}
